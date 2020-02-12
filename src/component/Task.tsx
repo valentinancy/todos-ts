cc4922d2ae58103a9f5861b8a236ce8ca079477a
Nancy Valentina (ID) <nancy.valentina@HCIDs-MacBook-Pro-3.local>
2020-02-12 17:41:53 +0700

    initial commit

---
+++ src/component/Task.tsx
@@ -0,0 +1,65 @@
+import React, { FunctionComponent, useState } from 'react';
+import { ITask } from '../interfaces';
+
+import trashIcon from '../assets/trash.png';
+import plusIcon from '../assets/plus.png'
+
+interface ITaskComponentProps extends ITask {
+    addTaskToDirectory: (task: ITask) => void;
+    deleteTaskFromDirectory: (id: string) => void;
+    id: string;
+    isNewTask: boolean;
+    toggleChecked: (id: string) => void;
+}
+
+const Task: FunctionComponent<ITaskComponentProps> = (props) => {
+    const { addTaskToDirectory, description, deleteTaskFromDirectory, id, isChecked, isNewTask, toggleChecked } = props;
+    const [isAddingTask, setIsAddingTask] = useState(false);
+    const [newTaskInput, setNewTaskInput] = useState('');
+
+    if(isNewTask) {
+        const taskDescriptionElement = document.getElementById('task-description-new');
+        if(taskDescriptionElement) {
+            if(isAddingTask) {
+                taskDescriptionElement.style.display = "none"
+            } else {
+                taskDescriptionElement.style.display = "block"
+            }
+        }
+    }
+    const task: ITask = {
+        description: newTaskInput,
+        isChecked: false
+    }
+    const handleAddTask = () => {
+        addTaskToDirectory(task);
+        setNewTaskInput('');
+        setIsAddingTask(false);
+    }
+    
+    const handleDeleteTask = (id: string) => {
+        deleteTaskFromDirectory(id);
+    }
+
+    const handleToggleChecked = (e: any, id: string) => {
+        toggleChecked(id)
+    }
+
+    return <>
+        <div className={`task-container ${isChecked ? 'disabled-container' : ''}`}>
+            <div className="task-checker">
+                {!isNewTask ? <input className="task-checkbox" type="checkbox" name="checked" onChange={(e) => handleToggleChecked(e, id)} checked={isChecked}/> : <img src={plusIcon} onClick={() =>setIsAddingTask(true)} className="new-task-add-icon" />}
+            </div>
+            <div className="task-details">
+                {(isNewTask && isAddingTask) && <input className="task-description-input" type="text" value={newTaskInput} onChange={(e) => setNewTaskInput(e.target.value)} />}
+                <p id={`task-description${isNewTask ? '-new' : ''}`} className={`task-description ${isNewTask ? 'disabled' : ''}${isAddingTask ? ' hide' : ''} ${isChecked ? 'disabled-text strike-text' : ''}`} onClick={() =>{isNewTask && setIsAddingTask(true)}} >{description}</p>
+            </div>
+            <div className="task-remover">
+                {!isNewTask && <img src={trashIcon} className="task-remove-icon" onClick={() => handleDeleteTask(id)} />}
+                {(isNewTask && isAddingTask) && <button className="task-add-button" onClick={() => handleAddTask()}>Add</button>}
+            </div>
+        </div>
+    </>
+}
+
+export default Task;
---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface ITask {
+    isChecked: boolean;
+    description: string;
+}
+
+export interface IDirectoryTasks {
+    [id: string]: ITask;
+}
